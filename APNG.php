d3dd65959ec3de965bc518f49673368e652ef2ef
mudkipme <imudkip@me.com>
2013-06-08 01:27:23 +0800

    Initial commit.

---
+++ APNG.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * APNG extension - Enables animated PNG support in Webkit browsers and Internet Explorer 10
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) { die(); }
+
+// credits
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'APNG',
+	'version' => '0.1',
+	'author' => array( 'David Mzareulyan', 'Mudkip' ),
+	'url' => 'https://github.com/mudkipme/mediawiki-apng',
+	'descriptionmsg'  => 'apng-desc',
+);
+
+$wgExtensionMessagesFiles['APNG'] = dirname( __FILE__ ). '/APNG.i18n.php';
+
+$wgResourceModules['ext.apng'] = array(
+    'scripts' => array('udeferred.js', 'crc32.js', 'apng-canvas.js', 'apng-loader.js' ),
+    'dependencies' => array( 'jquery', 'mediawiki' ),
+	'localBasePath' => dirname( __FILE__ ) . '/modules',
+	'remoteExtPath' => 'APNG/modules'
+);
+
+$wgHooks['ImageBeforeProduceHTML'][] = function($skin, $title, $file, &$frameParams, $handlerParams, $time, $res){
+    if ( $file ) {
+        $metadata = @unserialize($file->getMetadata());
+        $mimetype = $file->getMimeType();
+        if ( $mimetype == 'image/png' && isset($metadata['frameCount']) && $metadata['frameCount'] > 1 ) {
+            if ( !isset($frameParams['class']) ) {
+                $frameParams['class'] = 'apng';
+            } else {
+                $frameParams['class'] .= ' apng';
+            }
+        }
+    }
+    return true;
+};
+
+$wgHooks['BeforePageDisplay'][] = function($out, $skin){
+    $out->addModules( 'ext.apng' );
+    return true;
+};
---
+++ modules/apng-loader.js
@@ -0,0 +1,22 @@
+/**
+ * @preserve
+ * APNG-loader
+ *
+ * @author Mudkip Me
+ * @copyright 2013 Mudkip Me
+ * @link https://github.com/mudkipme/mediawiki-apng
+ * @license https://github.com/mudkipme/mediawiki-apng/blob/master/LICENSE (MIT License)
+ */
+$(function(){
+    var apng = $('img.apng[src$=".png"], .apng img[src$=".png"]');
+    if (mw.config.get('wgCanonicalNamespace') == 'File' && mw.config.get('wgTitle').match(/\.png$/) && $('.mw-noanimatethumb').size() > 0) {
+        apng = apng.add('#file img');
+    }
+    if (apng.size() > 0) {
+        APNG.ifNeeded(function() {
+            apng.each(function(i, img) {
+                APNG.animateImage(img);
+            });
+        });
+    }
+});
